4715845579c434412216b1702df62182dc494bd8
debora.maciel@techer.com.br <debora.maciel@techer.com.br>
2022-07-11 10:34:17 -0300

    Adding files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { HomeComponent } from './pages/home/home.component';
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: AppComponent, children: [
+      {
+        path:'', component: HomeComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
